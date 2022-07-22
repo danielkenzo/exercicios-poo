bb12caf4e3d45c027bc6e6bb80548906998ed873
Daniel Kenzo <80485432+danielkenzo@users.noreply.github.com>
2022-07-21 22:46:23 -0300

    Add Aula.java

---
+++ ep-plano-de-aulas/plano/Aula.java
@@ -0,0 +1,16 @@
+package plano;
+
+public abstract class Aula {
+
+	private String descricao;
+	
+	protected void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	public abstract String getTipo();
+}
